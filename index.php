55960db07f442126cf6d6638834956cd3cf6a110
halloShendy <shendypratama.work@gmail.com>
2021-01-02 22:15:13 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
 
     <h1>Hallo GIT</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid porro consequuntur consectetur distinctio asperiores. Veritatis excepturi eum, ducimus nisi eos porro magnam esse ad explicabo tenetur quisquam, deleniti culpa ab?</p>
+    <ul>
+        <li>list 1</li>
+        <li>list 2</li>
+        <li>list 3</li>
+    </ul>
 
 </body>
 
